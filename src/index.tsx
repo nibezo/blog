79862643a13cf12067729e459e29d9983f4eb891
Омурбек Медетов <omurbekmedetov5@gmail.com>
2022-12-15 21:00:31 +0300

    blog

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as ReactDOMClient from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App/App';
+import App from './components/app/App';
 import './index.scss';
 import { store } from './redux/store';
 
